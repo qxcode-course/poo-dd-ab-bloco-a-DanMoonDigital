82fb590f82e1df98e351646b82fbe9a07e53ab5f
Dan <pedrodanielmota@gmail.com>
2025-09-29 15:27:38 -0300

    Refazendo toda Loucura que Deu errado, refazer Toalha, animal e ir para o carro

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -4,4 +4,20 @@ class Towel:
         self.size: str = size
         self.wetness: int = 0
 
-# print("Olá, testando novamente por que a ultima pasta deu tudo errado :D")
+    def dry(self, amount: int):
+        self.wetness += amount
+        if self.wetness > self.getMaxWetness():
+            print("Toalha enchargada")
+            self.wetness = self.getMaxWetness()
+
+    def wrintOut(self):
+        self.wetness = 0
+
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20 
+        if self.size == "G":
+            return 30
+        return 0
