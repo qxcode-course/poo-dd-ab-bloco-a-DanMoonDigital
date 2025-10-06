6719485fc63d7dade018db096bafcae536453117
Dan <pedrodanielmota@gmail.com>
2025-10-05 23:22:29 -0300

    Press Start to Retry – You Got This! EU CONSIGOOOO

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,2 +1,18 @@
 class Animal:
-    def __init__
+    def __init__(self, species: str, sound: str):
+        self.species: str = species
+        self.sound: str = sound
+        self.age: int = 0
+
+        
+
+
+
+
+
+def main():
+    animal = Animal ("", "")
+    while True:
+        line: str = input ()
+        print("$" + line)
+        args: list [str] = line.split(" ")
--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -4,6 +4,15 @@ class Towel:
         self.size: str = size
         self.wetness: int = 0
 
+    def getMaxWetness(self) -> int:
+        if self.size == "P":
+            return 10
+        if self.size == "M":
+            return 20
+        if self.size == "G":
+            return 30
+        return 0
+
     def dry(self, amount: int):
         self.wetness += amount
         if self.wetness > self.getMaxWetness():
@@ -13,44 +22,21 @@ class Towel:
     def wringOut(self):
         self.wetness = 0
 
-    def getMaxWetness(self) -> int:
-        if self.size =="P":
-            return 10
-        if self.size =="M":
-            return 20
-        if self.size =="G":
-            return 30
-        return 0
+    def isDry(self) -> bool:
+        return self.wetness == 0
+    
+    def show(self):
+        print(self)
 
     def __str__(self) -> str:
         return f"Cor:{self.color}, Tam:{self.size}, Umidade:{self.wetness}"
     
-azul = Towel("blue", "G")
-print(azul.size == "G")
-print()
-
-def main():
-    toalha = Towel("", "")
-    while True:
-        print("$", end="")
-        line: str = input()
-        args: list [str] = line.split(" ")
-
-        if args[0] == "end":
-            break
-        elif args [0] == "new": 
-            color = args [1]
-            size = args [2]
-            toalha = Towel(color, size)
-        elif args [0] == "show":
-            print(toalha)
-        elif args[0] == "dry":
-            amount = int(args [1])
-            toalha.dry(amount)
-        else:
-            print("fail: comando desconhecido")
-
-main()
-
-
-
+toalha = Towel("Azul", "G")
+toalha.show()
+toalha.dry(10)
+toalha.show()
+print(toalha.isDry())
+toalha.dry(10)
+toalha.show
+toalha.wringOut()
+toalha.show()
